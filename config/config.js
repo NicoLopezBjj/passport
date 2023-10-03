62e1e010709fa3dac9b25888723b821b151a59cd
NicoLopezBjj <lopeznnr@gmail.com>
2023-10-03 12:25:18 -0300

    add config.js and keys.js, GoogleStrategy

--- config/config.js
+++ config/config.js
@@ -4,6 +4,9 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 passport.use(
     new GoogleStrategy({
         // opciones de estrategias de google
+        clientID:keys.google.clientID,
+        clientSecret:keys.google.clientSecret,
+        callbackURL:'auth/google/redirect',
     },()=>{
         // funcion callback de passport
 
