c9dd8276f8655065ce7db6183548f170f7ba9947
NicoLopezBjj <lopeznnr@gmail.com>
2023-10-03 12:32:16 -0300

    modified config and routes

---
+++ routes/routesAuth.js
@@ -0,0 +1,16 @@
+const router = require ('express').Router()
+const passport=require('passport')
+
+router.get('/login'), (req,res)=>{
+    res.render('/login',{user:req.user})
+}
+
+router.get('/login'), (req,res)=>{
+    res.send('se deslogueo')
+}
+
+router.get('/google',passport.authenticate('google',{
+    scope:['profile']
+   }))
+
+module.exports= router
