ed3e9cc8fcd0f44f4f4222e7c28f0fa13ec237af
NicoLopezBjj <lopeznnr@gmail.com>
2023-10-10 09:30:55 -0300

    add cookie-sesion

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const rutasAuth=require('./routes/routesAuth')
 const passportSetUp=require('./config/config')
 const keys= require('./config/keys')
 const mongoose=require('mongoose')
+const cookieSession=require('cookie-session')
 
 
 const app = express()
 
 app.set('view engine','ejs')
+
+app.use(cookieSession({
+    maxAge:24*60*1000,
+    keys:[keys.session.cookieKey]
+}
+))
 
 const connectToMongoDB = async () => {
     try {
